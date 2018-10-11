1bb90d24b2d221d72eeb0901a578d553881c9d1d
Ruben Cruz <ssrcruz@gmail.com>
2018-10-11 15:21:32 -0400

    create the ShoppingCart component and interact with the Vuex state through getters

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,22 @@ export default new Vuex.Store({
   getters: { // = computed properties
     availableProducts (state, getters) {
       return state.products.filter(product => product.inventory > 0)
+    },
+
+    cartProducts (state) {
+      return state.cart.map(cartItem => {
+        const product = state.products.find(product => product.id === cartItem.id)
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity
+        }
+      })
+    },
+
+    cartTotal (state, getters) { // Sums up the total price of all the items in the cart
+      let total = 0
+      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
     }
   },
 
